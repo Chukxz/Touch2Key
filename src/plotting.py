528487e3a640c2f97e2535a507f86bcac164cd5d
Chukwuemeka Igwe <109679877+Chukxz@users.noreply.github.com>
2026-01-15 20:47:26 +0100

    Update plotting.py

--- src/plotting.py
+++ src/plotting.py
@@ -557,7 +557,7 @@ class Plotter:
                         print(f"[!] ERROR: Sprint point must be OUTSIDE the joystick radius!")
                     return False, id
 
-                    self.sprint_distance = dy
+                    self.sprint_distance = actual_dist
                     self.saved_sprint_distance = True
                 
             elif self.mode == RECT:
